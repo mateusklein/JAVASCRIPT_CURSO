319512479d8e8c41bb14a59c1648046f13600421
mateus klein <mateusklein@terra.com.br>
2023-07-01 15:17:16 -0300

    new commit

---
+++ 01-calcula_media.js
@@ -0,0 +1,11 @@
+function calcula_media(){
+    var total = 0;
+    var qtd = arguments.length;
+    var x=0;
+    while(typeof arguments[x] === "number"){
+        total += arguments[x];
+        x++;
+    }
+    return total/qtd;
+}
+
